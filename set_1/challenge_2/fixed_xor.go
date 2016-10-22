c503544f619c71588906331effbf0f959d007c91
Nick Klauer <klauer@gmail.com>
2016-10-22 17:00:46 -0500

    refactoring for some simplicity

--- set_1/challenge_2/fixed_xor.go
+++ set_1/challenge_2/fixed_xor.go
@@ -1,5 +1,23 @@
 package main
 
+import "encoding/hex"
+import "github.com/pkg/errors"
+
+func FixedXorString(input, inXor string) ([]byte, error) {
+	if len(input) != len(inXor) {
+		return []byte(""), errors.New("buffers not the same length")
+	}
+	inBytes, err := hex.DecodeString(input)
+	if err != nil {
+		return []byte(""), err
+	}
+	xorBytes, err := hex.DecodeString(inXor)
+	if err != nil {
+		return []byte(""), err
+	}
+	return FixedXor(inBytes, xorBytes), nil
+}
+
 func FixedXor(input, inXor []byte) []byte {
 	length := len(input)
 	output := make([]byte, length)
--- set_1/challenge_2/fixed_xor_test.go
+++ set_1/challenge_2/fixed_xor_test.go
@@ -8,20 +8,17 @@ import (
 )
 
 func TestFixedXor(t *testing.T) {
-	input, err := hex.DecodeString("1c0111001f010100061a024b53535009181c")
-	if err != nil {
-		t.Fatalf("Expected to decode string, did not")
-	}
-	inXor, err := hex.DecodeString("686974207468652062756c6c277320657965")
-	if err != nil {
-		t.Fatalf("Expected to decode string, did not")
-	}
+	input := "1c0111001f010100061a024b53535009181c"
+	inXor := "686974207468652062756c6c277320657965"
 	expected, err := hex.DecodeString("746865206b696420646f6e277420706c6179")
 	if err != nil {
 		t.Fatalf("Expected to decode string, did not")
 	}
 
-	actual := FixedXor(input, inXor)
+	actual, err := FixedXorString(input, inXor)
+	if err != nil {
+		t.Fatalf("Error in parsing: %v", err)
+	}
 	fmt.Printf("%s\n", actual)
 	comp := bytes.Compare([]byte(expected), actual)
 	if comp != 0 {
