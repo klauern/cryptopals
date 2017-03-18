77f346b26a3cfe09756b866e64cbaff6d1d011d7
Nick Klauer <klauer@gmail.com>
2017-03-18 13:35:16 -0500

    cheat when calc'ing hamming distance
    I don't know enough about the underlying sizes of the Go type system, but this awesome library does. github.com/steakknife/hamming has a lot of helper functions, and even has one that is cheating in a way, because it would simplify the for ... range loop I wrote, but I want to at least pretend to do something useful here

--- set_1/challenge_6/break_repeating_key_xor.go
+++ set_1/challenge_6/break_repeating_key_xor.go
@@ -5,11 +5,10 @@ import "github.com/steakknife/hamming"
 // HammingDistance calculates the distance of two strings based two strings for comparison.
 // It returns the overall distance as an average.
 func HammingDistance(from, to string) int {
-	// take the first keysize of bytes against the second keysize of bytes
-	var dist = make([]int, len(from))
+	// var dist = make([]int, len(from))
 	var tot int
 	for i := range from {
-		dist[i] = hamming.Byte(from[i], to[i])
+		// dist[i] = hamming.Byte(from[i], to[i])
 		tot += hamming.Byte(from[i], to[i])
 	}
 	return tot
